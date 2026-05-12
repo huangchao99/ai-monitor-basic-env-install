52b0b4704e28ca311aab7f5e1cfbc82b9c4c2f3f
HuangChao <2170189105@qq.com>
2026-05-12 11:21:39 +0800

    commit

--- get_package_from_github.sh
+++ get_package_from_github.sh
@@ -2,6 +2,7 @@
 set -e
 
 # 依赖包下载链接（替换成你实际的 Releases 链接）
+
 DEPS_URL="https://github.com/huangchao99/ai-monitor-basic-env-install/releases/download/v1.0.0/basic-env-deps.tar.gz"
 
 echo "正在下载基础环境依赖包..."
